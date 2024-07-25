8897c4ece2f7528274be2d1f0fdca9788e22580e
ALEXX4891 <alexx4891@mail.ru>
2024-07-25 15:04:54 +0500

    добавит range-slider

--- dist/js/app.js
+++ dist/js/app.js
@@ -37,8 +37,145 @@ navLinks.forEach((item) => {
 //     clickable: true,
 //   },
 // });
+// -------------------------------------------- start range-slider: ---------------------------------------------
+const rangeSlider = document.querySelector(".range-slider");
+// текстовые инпуты:
+const priceInputs = document.querySelectorAll(".price-inputs-wrap input");
+const textInputMin = document.querySelector(".select__input_from");
+const textInputMax = document.querySelector(".select__input_to");
 
+// рендж инпуты:
+const rangeInputs = document.querySelectorAll(".range-inputs-wrap input");
+const rangeInputMin = document.querySelector(".min-range");
+const rangeInputMax = document.querySelector(".max-range");
 
+// основные параметры:
+const priceGap = 1;
+const minRange = 26;
+const maxRange = 81;
+
+// присваиваем значения инпутам:
+textInputMin.value = minRange;
+textInputMax.value = maxRange;
+rangeInputMin.value = minRange;
+rangeInputMax.value = maxRange;
+
+textInputMin.min = minRange;
+textInputMin.min = minRange;
+rangeInputMin.min = minRange;
+rangeInputMax.min = minRange;
+
+textInputMin.max = maxRange;
+textInputMax.max = maxRange;
+rangeInputMin.max = maxRange;
+rangeInputMax.max = maxRange;
+
+textInputMin.step = priceGap;
+textInputMax.step = priceGap;
+rangeInputMin.step = priceGap;
+rangeInputMax.step = priceGap;
+
+// обработка событий текстовых инпутов:
+priceInputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    //получаем значения из текстовых инпутов:
+    let minVal = parseInt(textInputMin.value);
+    let maxVal = parseInt(textInputMax.value);
+    let diff = maxVal - minVal;
+
+    // ограничиваем значение min инпута:
+    if (minVal < minRange) {
+      textInputMin.value = minRange;
+      minVal = minRange;
+    }
+
+    // ограничиваем значение max инпута:
+    if (maxVal > maxRange) {
+      textInputMax.value = maxRange;
+      maxVal = maxRange;
+    }
+
+    // ограничиваем максимальное значение min инпута:
+    if (e.target === textInputMin) {
+      if (minVal > maxVal - priceGap) {
+        textInputMin.value = maxVal - priceGap;
+        minVal = maxVal - priceGap;
+      }
+    }
+
+    // ограничиваем минимальное значение max инпута:
+    if (e.target === textInputMax) {
+      if (maxVal < minVal + priceGap) {
+        textInputMax.value = minVal + priceGap;
+        maxVal = minVal + priceGap;
+      }
+    }
+
+    // вычисляем положение рендж инпутов:
+    if (diff >= priceGap) {
+      rangeInputMax.value = maxVal;
+      rangeInputMin.value = minVal;
+      rangeSlider.style.right = `${
+        100 - ((maxVal - minRange) * 100) / (maxRange - minRange)
+      }%`;
+      rangeSlider.style.left = `${
+        ((minVal - minRange) * 100) / (maxRange - minRange)
+      }%`;      
+    }
+  });
+});
+
+rangeInputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    //получаем значения из текстовых инпутов:
+    let minVal = parseInt(rangeInputMin.value);
+    let maxVal = parseInt(rangeInputMax.value);
+    let diff = maxVal - minVal;
+
+    // ограничиваем значение min инпута:
+    if (minVal < minRange) {
+      rangeInputMin.value = minRange;
+      minVal = minRange;      
+    }
+
+    // ограничиваем значение max инпута:
+    if (maxVal > maxRange) {
+      rangeInputMax.value = maxRange;
+      maxVal = maxRange;
+      
+    }
+
+    // ограничиваем максимальное значение min инпута:
+    if (e.target === rangeInputMin) {
+      if (minVal > maxVal - priceGap) {
+        rangeInputMin.value = maxVal - priceGap;
+        minVal = maxVal - priceGap;        
+      }
+    }
+
+    // ограничиваем минимальное значение max инпута:
+    if (e.target === rangeInputMax) {
+      if (maxVal < minVal + priceGap) {
+        rangeInputMax.value = minVal + priceGap;
+        maxVal = minVal + priceGap;        
+      }
+    }
+
+    // вычисляем положение рендж инпутов:
+    if (diff >= priceGap) {
+      textInputMin.value = minVal;
+      textInputMax.value = maxVal;
+      rangeSlider.style.right = `${
+        100 - ((maxVal - minRange) * 100) / (maxRange - minRange)
+      }%`;
+      rangeSlider.style.left = `${
+        ((minVal - minRange) * 100) / (maxRange - minRange)
+      }%`;      
+    }
+  });
+});
+
+// -------------------------------------------- end range-slider: ---------------------------------------------
 // -------------------------------------------- start slider: ---------------------------------------------
 new Swiper(".news-slider_swiper", {
   // Optional parameters
